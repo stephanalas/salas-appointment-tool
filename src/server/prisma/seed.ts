e9ea7e801c7299005337761d0e7c9cd62cbfdb3d
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-06-16 10:17:00 -0400

    seed configured

--- src/server/prisma/seed.ts
+++ src/server/prisma/seed.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 async function main() {
@@ -15,17 +14,73 @@ async function main() {
       password: "1234",
     },
   });
-  // profiles
-  // const prospect = await prisma.profile.upsert({
-  //   where: {
-  //     email: "prospect@mail.com",
-  //   },
-  //   update: {},
-  //   create: {
-  //     phone: "1234567890",
-  //     email: "prospect@mail.com",
-  //     industry: "tech",
-  //     location: "nyc",
-  //   },
-  // });
+  // 2 profiles, 1 task each, 1 subtask each task
+  const prospect = await prisma.profile.upsert({
+    where: {
+      email: "prospect@mail.com",
+    },
+    update: {},
+    create: {
+      phone: "1234567890",
+      email: "prospect@mail.com",
+      industry: "tech",
+      location: "nyc",
+      stage: "PROSPECT",
+      notes: "notes regarding prospect",
+      active: true,
+      tasks: {
+        create: {
+          subtasksComplete: false,
+          deadline: new Date("2023-06-19"),
+          urgency: "URGENT",
+          userId: admin.id,
+          subtasks: {
+            create: {
+              description: "sub task to do",
+              completed: false,
+            },
+          },
+        },
+      },
+    },
+  });
+  const client = await prisma.profile.upsert({
+    where: {
+      email: "client@mail.com",
+    },
+    update: {},
+    create: {
+      phone: "1234567890",
+      email: "client@mail.com",
+      industry: "startup",
+      location: "philly",
+      stage: "CLIENT",
+      notes: "notes regarding client",
+      active: true,
+      tasks: {
+        create: {
+          deadline: new Date("2023-06-19"),
+          urgency: "LOW",
+          userId: admin.id,
+          subtasksComplete: false,
+          subtasks: {
+            create: {
+              description: "sub task to do",
+              completed: false,
+            },
+          },
+        },
+      },
+    },
+  });
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
