8e28974ae8f522b7c7729a4e63bb12bb58765e6c
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-06-18 06:08:53 -0400

    login form

--- src/client/routes/Login.tsx
+++ src/client/routes/Login.tsx
@@ -1,17 +1,41 @@
+import { useState, FormEventHandler } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-
+import { useLoginMutation } from "../store/api";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, result] = useLoginMutation();
+  const handleSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+    console.log("email", email);
+    console.log("password", password);
+    try {
+      await login({ email, password });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Login</Form.Label>
       </Form.Group>
       <Form.Group className="mb-3">
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </Form.Group>
       <Form.Group className="mb-3">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
