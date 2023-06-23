6f61e60276e1f400687ba456f2ddab63e5439d2e
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-06-23 16:00:57 -0400

    added theme

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,16 @@
-import BootstrapNavbar from "./common/BootstrapNavbar";
+import BootstrapNavbar from "./common/Navbar";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAppSelector } from "./hooks";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 const App = () => {
   const user = useAppSelector((store) => store.auth.user);
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <BootstrapNavbar />
       {!user && <Navigate to="/login" />}
       <Outlet />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/client/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#c3511a",
+    },
+    secondary: {
+      main: "#187aaf",
+    },
+  },
+});
+
+export default theme;
