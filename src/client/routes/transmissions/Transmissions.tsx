027aeda48e02b4b82d14566df893171ae9d0063a
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-09-28 15:34:30 -0400

    TODO: Transmission table

--- src/client/routes/transmissions/Transmissions.tsx
+++ src/client/routes/transmissions/Transmissions.tsx
@@ -1,4 +1,6 @@
 const Transmissions = () => {
+  // TODO: implement transmission table with appropriate columns
+  // rtk hook to bring in transmissions
   return <div>Under construction</div>;
 };
 
