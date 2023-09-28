c37c915714514caaeed9d5919a90467c4ac98ec5
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-09-28 15:10:59 -0400

    nodemailer transport

--- src/server/mailer.ts
+++ src/server/mailer.ts
@@ -9,26 +9,4 @@ const transporter = nodemailer.createTransport({
   // secure: false,
 });
 
-console.log("this is transporter", transporter);
-
-async function sendTestMail() {
-  try {
-    const mailOptions = {
-      from: process.env.EMAIL_FROM,
-      to: process.env.TEST_EMAIL,
-      subject: "Test email",
-      text: "testing email from nodemailer",
-    };
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: " + info.response);
-    console.log(info.envelope);
-    console.log("accepted", info.accepted);
-    console.log("pending", info.pending);
-    console.log("rejected", info.rejected);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// sendTestMail();
 export default transporter;
