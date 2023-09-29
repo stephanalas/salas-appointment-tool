9a911b44d71bfc668321538f6f56ecb83dbb0ed7
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-09-29 09:44:21 -0400

    transmissions table wip

--- src/client/routes/transmissions/Transmissions.tsx
+++ src/client/routes/transmissions/Transmissions.tsx
@@ -1,7 +1,56 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useGetAllTransmissionsQuery } from "../../store/api";
+
+const columns: GridColDef[] = [
+  // rows here
+];
+
 const Transmissions = () => {
-  // TODO: implement transmission table with appropriate columns
-  // rtk hook to bring in transmissions
-  return <div>Under construction</div>;
+  const { data, isLoading } = useGetAllTransmissionsQuery();
+
+  return (
+    <Grid
+      item
+      container
+      direction="column"
+      justifyContent="center"
+      sx={{ padding: "1rem" }}
+    >
+      <Grid
+        item
+        container
+        justifyContent="space-between"
+        sx={{
+          marginBottom: "1rem",
+        }}
+      >
+        <Grid item>
+          <Typography variant="h5">Transmissions</Typography>
+        </Grid>
+      </Grid>
+      <Grid item sx={{ height: "50vh", width: "100%" }}>
+        {isLoading ? (
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height={"50vh"}
+            sx={{
+              padding: "1rem",
+            }}
+          />
+        ) : (
+          <DataGrid
+            columns={columns}
+            rows={[]} // rows need to be transform for columns in api
+            // onRowClick={handleRowClick}
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Transmissions;
