4e6cd15fdcfa5a25b6d50a766deb83837cc497fa
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-09-29 18:52:51 -0400

    format transmission on server wip

--- src/server/api/transmissions.ts
+++ src/server/api/transmissions.ts
@@ -2,9 +2,42 @@ import express from "express";
 import prisma from "../prisma/primsa.ts";
 const transmissionRouter = express.Router();
 
+// wrote in two places. types could be shared between client and server using path mapping.
+enum TransmissionStatus {
+  SUCCESS,
+  FAILED,
+}
+
+interface Transmission {
+  id: number;
+  sentTo: string;
+  isAppointment: boolean;
+  transmissionType: string;
+  date: string;
+  time: string;
+  status: TransmissionStatus;
+}
+
 transmissionRouter.get("/", async (_req, res, next) => {
   try {
     const transmissions = await prisma.transmission.findMany();
+    // format data
+    // findMany should include profiles associated with transmissions
+    //
+
+    const formattedTransmissions = transmissions.map((transmission) => {
+      // separate time and date
+      return {
+        id: transmission.id,
+        // sentTo
+        isAppointment: transmission.isAppointment,
+        transmissionType: transmission.transmissionType,
+        // date
+        // time
+        status: transmission.status,
+      };
+    });
+
     res.send(transmissions);
   } catch (error) {
     next(error);
